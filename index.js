86612a1a5c4c7a0fbfe9ac74d44cffdaa86b37f8
vj_akash <48998005+vjakash@users.noreply.github.com>
2020-06-16 20:34:41 +0530

    db integrated

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const port = process.env.PORT || 3000
 
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongodb = require('mongodb');
+const mongoClient = mongodb.MongoClient;
+const dbURL = `mongodb://localhost:27017`;
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors({
+    origin: '*'
+}));
 
 var morgan = require('morgan')
 app.use(morgan(':method Request :url status code :status  res-time :response-time ms'));
@@ -23,18 +28,30 @@ app.post('/mentor', (req, res) => {
             msg: "Name or Phoneno missing"
         })
     else {
-        mentor.push({
-            id: mentorId,
-            name: req.body.name,
-            age: req.body.age,
-            role: req.body.role,
-            phoneNo: req.body.phoneNo,
-            students: []
-        })
-        mentorId++;
-        res.status(200).json({
-            msg: "Successfully added mentor"
-        })
+        let obj = req.body;
+        obj["students"] = [];
+        mongoClient.connect(dbURL, (err, client) => {
+                if (err) throw err;
+                let db = client.db('mentorAssign');
+                db.collection("mentor").insertOne(obj, (err, data) => {
+                    if (err) throw err;
+                    client.close();
+                    res.status(200).json({
+                        msg: "Successfully added mentor"
+                    })
+                })
+
+            })
+            // mentor.push({
+            //     id: mentorId,
+            //     name: req.body.name,
+            //     age: req.body.age,
+            //     role: req.body.role,
+            //     phoneNo: req.body.phoneNo,
+            //     students: []
+            // })
+            // mentorId++;
+
     }
 })
 
@@ -44,18 +61,30 @@ app.post('/students', (req, res) => {
             msg: "Name or Batch or Phone number missing."
         })
     else {
-        student.push({
-            id: studentId,
-            name: req.body.name,
-            batchNo: req.body.batchNo,
-            course: req.body.course,
-            phoneNo: req.body.phoneNo,
-            mentor: ""
-        })
-        studentId++;
-        res.status(200).json({
-            msg: "Successfully added student"
-        })
+        let obj = req.body;
+        obj["mentor"] = "";
+        mongoClient.connect(dbURL, (err, client) => {
+                if (err) throw err;
+                let db = client.db('mentorAssign');
+                db.collection("student").insertOne(req.body, (err, data) => {
+                    if (err) throw err;
+                    client.close();
+                    res.status(200).json({
+                        msg: "Successfully added student"
+                    })
+                })
+
+            })
+            // student.push({
+            //     id: studentId,
+            //     name: req.body.name,
+            //     batchNo: req.body.batchNo,
+            //     course: req.body.course,
+            //     phoneNo: req.body.phoneNo,
+            //     mentor: ""
+            // })
+            // studentId++;
+
     }
 })
 
@@ -66,17 +95,29 @@ app.post('/assignstudent', (req, res) => {
             msg: "Mentor or Students missing."
         })
     else {
-        let mentors = req.body.mentorId;
+        // let mentors = req.body.mentorId;
+        let mentors = mongodb.ObjectID(req.body.mentorId);
         let studentIds = req.body.students;
         for (let i of studentIds) {
-            student[i].mentor = JSON.parse(JSON.stringify(mentor[mentors]));
-            mentor[mentors].students.push(JSON.parse(JSON.stringify(student[i])))
+            i = mongodb.ObjectID(i);
+            // student[i].mentor = JSON.parse(JSON.stringify(mentor[mentors]));
+            // mentor[mentors].students.push(JSON.parse(JSON.stringify(student[i])))
+            mongoClient.connect(dbURL, (err, client) => {
+                if (err) throw err;
+                let db = client.db('mentorAssign');
+                db.collection("student").updateOne({ _id: i }, { $set: { mentor: mentors } }, (err, data) => {
+                    if (err) throw err;
+                    db.collection("mentor").updateOne({ _id: mentors }, { $push: { students: i } }, (err, data) => {
+                        if (err) throw err;
+                        client.close();
+                        res.status(200).json({
+                            msg: "Successfully assigned students to mentor!."
+                        })
+                    })
+                })
+            })
+
         }
-        console.log(mentor);
-        console.log(student);
-        res.status(200).json({
-            msg: "Successfully assigned students to mentor!."
-        })
     }
 })
 
@@ -86,23 +127,43 @@ app.put('/update', (req, res) => {
             msg: "Mentor or Students missing."
         })
     else {
-        let mid = req.body.mentorId
-        let sid = req.body.studentId
-        if (student[sid].mentor != '') {
-            //remove old mentor
-            let oldId = student[sid].mentor.id
-            for (let i = 0; i < mentor[oldId].students.length; i++)
-                if (mentor[oldId].students[i].id == sid) {
-                    mentor[oldId].students.splice(i, 1);
-                    break;
-                }
-        }
-        //update new mentor
-        student[sid].mentor = mentor[mid]
-        mentor[mid].students.push(student[sid])
+        let mid = mongodb.ObjectID(req.body.mentorId);
+        let sid = mongodb.ObjectID(req.body.studentId);
 
-        res.status(200).json({
-            msg: 'Updated the mentor'
+        mongoClient.connect(dbURL, (err, client) => {
+            if (err) throw err;
+            let db = client.db('mentorAssign');
+            db.collection('student').aggregate([
+                { $match: { _id: sid } }, {
+                    $lookup: {
+                        from: 'mentor',
+                        localField: 'mentor',
+                        foreignField: '_id',
+                        as: 'mentor'
+                    }
+                }
+            ]).toArray((err, data) => {
+                if (err) throw err;
+                // res.json(data);
+                // console.log(data[0].mentor[0]);
+                if (data.mentor != '') {
+                    //remove old mentor
+                    let oldId = mongodb.ObjectID(data[0].mentor[0]["_id"]);
+                    db.collection("mentor").update({ _id: oldId }, { $pull: { students: sid } }, (err, data) => {
+                        if (err) throw err;
+                    })
+                }
+                db.collection("mentor").update({ _id: mid }, { $push: { students: sid } }, (err, data) => {
+                    if (err) throw err;
+                    db.collection("student").update({ _id: sid }, { $set: { mentor: mid } }, (err, data) => {
+                        if (err) throw err;
+                        client.close();
+                        res.status(200).json({
+                            msg: 'Updated the mentor'
+                        })
+                    })
+                })
+            })
         })
     }
 })
@@ -113,26 +174,79 @@ app.post('/liststudents', (req, res) => {
             msg: "Mentor Id missing."
         })
     else {
-        console.log(typeof(req.body.mentorId));
-        if (mentor.length == 0) {
-            res.json({
-                "msg": "No mentor available"
+        let mid = mongodb.ObjectID(req.body.mentorId);
+        mongoClient.connect(dbURL, (err, client) => {
+            if (err) throw err;
+            let db = client.db('mentorAssign');
+            db.collection("mentor").aggregate([{ $match: { _id: mid } }, {
+                $lookup: {
+                    from: 'student',
+                    localField: 'students',
+                    foreignField: '_id',
+                    as: 'students'
+                }
+            }]).toArray((err, data) => {
+                if (err) throw err;
+                console.log(data[0]['students']);
+                client.close();
+                res.status(200).json(data[0]['students'])
             })
-        } else {
-            console.log(mentor[req.body.mentorId]);
-            res.status(200).json(mentor[req.body.mentorId].students)
-        }
-
+        })
     }
 })
 app.get('/listofmentors', (req, res) => {
+    mongoClient.connect(dbURL, (err, client) => {
+        if (err) throw err;
+        let db = client.db('mentorAssign');
+        db.collection("mentor").aggregate([{
+            $lookup: {
+                from: 'student',
+                localField: 'students',
+                foreignField: '_id',
+                as: 'students'
+            }
+        }]).toArray((err, data) => {
+            if (err) throw err;
+            client.close();
+            res.status(200).json(data)
+        })
 
-    res.status(200).json(mentor);
+    })
+
 
 });
 app.get('/listofstudents', (req, res) => {
+    // dbo.collection('orders').aggregate([
+    //     { $lookup:
+    //        {
+    //          from: 'products',
+    //          localField: 'product_id',
+    //          foreignField: '_id',
+    //          as: 'orderdetails'
+    //        }
+    //      }
+    //     ]).toArray(function(err, res) {
+    //     if (err) throw err;
+    //     console.log(JSON.stringify(res));
+    //     db.close();
+    //   });
+    mongoClient.connect(dbURL, (err, client) => {
+        if (err) throw err;
+        let db = client.db('mentorAssign');
+        db.collection("student").aggregate([{
+            $lookup: {
+                from: 'mentor',
+                localField: 'mentor',
+                foreignField: '_id',
+                as: 'mentor'
+            }
+        }]).toArray((err, data) => {
+            if (err) throw err;
+            client.close();
+            res.status(200).json(data)
+        })
 
-    res.status(200).json(student);
+    })
 
 });
 app.listen(port, () => {
